af034f6dacd49db7b146601cadebaa7636eced1e
RianErick <rianzx34@gmail.com>
2024-02-15 17:54:28 -0300

    feat: criando estrututa do jogo de ordenacao

--- main.go
+++ main.go
@@ -9,4 +9,19 @@ import (
 func main() {
 	fmt.Println(player.Title())
 
+	type ArraySort struct {
+		stream []int
+		check  bool
+	}
+
+	var oneArray ArraySort
+	var twoArray ArraySort
+
+	oneArray.stream = []int{1, 4, 9, 7, 3, 2, 0}
+	oneArray.check = false
+
+	twoArray.stream = []int{1, 2, 4, 52, 2, 3, 0}
+	twoArray.check = true
+
+	//Logica para fazer o usuario escolher qual do array seria a escolha correta
 }
