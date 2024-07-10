54d3dbd34efee4b5ead3ca994ed5b0fd84358644
RianErick <rianzx34@gmail.com>
2024-07-10 12:09:15 -0300

    fix: refactor logic game array

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
-
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/RianErick/go-learning/game"
 	"github.com/RianErick/go-learning/utils"
@@ -18,49 +17,58 @@ func clearConsole() {
 	cmd.Run()
 }
 
-func main() {
-	fmt.Println(game.Title())
-
-	var arrayUser = make([]int, 10)
-	var arrayMemory0 = make([]int, 10)
-	var arrayMemory1 = make([]int, 10)
-
-	rand.Seed(int64(time.Now().Nanosecond()))
-
-	for i := 0; i < len(arrayUser); i++ {
-		arrayUser[i] = rand.Intn(100)
+func generateRandomArray(size int, max int) []int {
+	array := make([]int, size)
+	for i := 0; i < size; i++ {
+		array[i] = rand.Intn(max)
 	}
+	return array
+}
 
-	fmt.Printf("You have 10 seconds to memorize it, run: %v\n", arrayUser)
-
-	for i := 0; i < 10; i++ {
+func displayAndWait(array []int, duration time.Duration) {
+	fmt.Printf("You have %v seconds to memorize it, run: %v\n", duration.Seconds(), array)
+	for i := 0; i < int(duration.Seconds()); i++ {
 		time.Sleep(time.Second)
 		fmt.Printf("===")
 	}
 	clearConsole()
 	fmt.Println(game.Title())
+}
 
-	for i := 0; i < 10; i++ {
-		arrayMemory0[i] = rand.Intn(100)
-		for j := 0; j < 10; j++ {
-			arrayMemory1[i] = rand.Intn(100)
-		}
+func generateMultipleRandomArrays(count, size, max int) [][]int {
+	arrays := make([][]int, count)
+	for i := 0; i < count; i++ {
+		arrays[i] = generateRandomArray(size, max)
 	}
+	return arrays
+}
 
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println(game.Title())
+
+	arrayUser := generateRandomArray(10, 100)
+	displayAndWait(arrayUser, 10*time.Second)
+
+	arrayOptions := generateMultipleRandomArrays(5, 10, 100) // Generate 5 random options
 	arrayTrue := utils.SortList(arrayUser)
-	i0 := utils.SortList(arrayMemory0)
-	i1 := utils.SortList(arrayMemory1)
-
 	matrixOfArray := []utils.Matrix{
 		{Array: arrayTrue, Real: true},
-		{Array: i0, Real: false},
-		{Array: i1, Real: false},
 	}
-	var selectOption int
 
+	for _, arr := range arrayOptions {
+		sortedArr := utils.SortList(arr)
+		matrixOfArray = append(matrixOfArray, utils.Matrix{Array: sortedArr, Real: false})
+	}
+
+	rand.Shuffle(len(matrixOfArray), func(i, j int) {
+		matrixOfArray[i], matrixOfArray[j] = matrixOfArray[j], matrixOfArray[i]
+	})
+
+	var selectOption int
 	fmt.Println("Select option true:")
-	for i := 0; i < len(matrixOfArray); i++ {
-		fmt.Printf("%v : %v\n", i, matrixOfArray[i].Array)
+	for i, matrix := range matrixOfArray {
+		fmt.Printf("%v : %v\n", i, matrix.Array)
 	}
 	fmt.Scanf("%d", &selectOption)
 
@@ -69,5 +77,4 @@ func main() {
 	} else {
 		fmt.Println("F :0")
 	}
-
 }
